6f890a7e16d807c16d57e86813bc0c27518332f2
Jeremy Puchta <hello@jeremypuchta.com>
2024-04-16 13:52:27 +0200

    execute code in main method to check selection strategies

--- src/main/java/com/jeremypuchta/Main.java
+++ src/main/java/com/jeremypuchta/Main.java
@@ -1,12 +1,13 @@
 package com.jeremypuchta;
 
-import com.jeremypuchta.selections.RandomSelectionStrategy;
+import com.jeremypuchta.selections.RoundRobinSelectionStrategy;
 
 public class Main {
     public static void main(String[] args) {
-        LoadBalancer loadBalancer = new LoadBalancer(new RandomSelectionStrategy());
-        loadBalancer.register(new BackendInstance("1.2.3.4"));
-        loadBalancer.register(new BackendInstance("0.0.0.0"));
-        System.out.println(loadBalancer.getInstances().toString());
+        LoadBalancer loadBalancer = new LoadBalancer(new RoundRobinSelectionStrategy());
+        loadBalancer.register(new BackendInstance("128.168.1.1:80"));
+        loadBalancer.register(new BackendInstance("128.168.1.1:81"));
+        loadBalancer.register(new BackendInstance("128.168.1.1:82"));
+        loadBalancer.selectInstance().get().getAddress();
     }
 }
